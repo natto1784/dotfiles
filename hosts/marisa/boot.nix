bca3b147134086671dc2e60e69416ad8b195f299
natto1784 <natto@weirdnatto.in>
2022-01-31 22:42:32 +0530

    Marisa: boot changes

--- hosts/marisa/boot.nix
+++ hosts/marisa/boot.nix
@@ -1,15 +1,26 @@
-{config, ...}:
+{ config, pkgs, ... }:
 {
   boot = {
-    initrd.availableKernelModules = [ "xhci_pci" "usb_storage" "usbhid" "uas" "pcie-brcmstb"];
+    kernelParams = [ "console=ttyS0,115200n8" "console=tty0" ];
+    #    kernelPackages = pkgs.linuxPackages_rpi4; //this sucks
+    initrd.availableKernelModules = [ "xhci_pci" "usb_storage" "usbhid" "uas" "pcie-brcmstb" ];
     loader = {
       grub.enable = false;
- #     generic-extlinux-compatible.enable= true;
-      raspberryPi= {
+      generic-extlinux-compatible.enable = true;
+      /*raspberryPi= {
         version = 4;
         firmwareConfig = "dtparam=sd_poll_once=on";
         enable = true;  
-      };
+        };*/ #conflicts with generic-extlinux-comaptible
     };
+    /* kernelPatches = [
+      {
+      name = "change-pgtable";
+      patch = null;
+      extraConfig = ''
+      CONFIG_PGTABLE_LEVELS 4
+      '';
+      }
+      ];*/
   };
 }
