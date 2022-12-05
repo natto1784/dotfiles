d654ab946355f8cb54d5c58cb3a4188c2814cbf0
Amneesh Singh <natto@weirdnatto.in>
2022-12-05 23:47:58 +0530

    remove useless stuff
    Signed-off-by: Amneesh Singh <natto@weirdnatto.in>

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -2,9 +2,7 @@ final: prev:
 let
   call = prev.callPackage; in
 {
-  mpd_discord_richpresence = call ./mpd-rpc { };
   customscripts = call ./customscripts { };
-  gruvbox-icons = call ./gruvbox-icons { };
   mymacs = call ./emacs { };
   tlauncher = call ./tlauncher { };
   simp1e-cursors = call ./simp1e { };
