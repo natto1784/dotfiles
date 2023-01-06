15e6418361c4d21ecc34db8d436503ae469511b9
Amneesh Singh <natto@weirdnatto.in>
2023-01-07 05:11:43 +0530

    flake: remove devshell
    Signed-off-by: Amneesh Singh <natto@weirdnatto.in>

--- flake.nix
+++ flake.nix
@@ -3,18 +3,30 @@
 
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    home-manager.url = github:nix-community/home-manager;
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-parts.url = github:hercules-ci/flake-parts;
     nvim-overlay.url = github:nix-community/neovim-nightly-overlay;
-    mailserver.url = gitlab:simple-nixos-mailserver/nixos-mailserver;
+    mailserver = {
+      url = gitlab:simple-nixos-mailserver/nixos-mailserver;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     emacs-overlay.url = github:nix-community/emacs-overlay;
     rust-overlay.url = github:oxalica/rust-overlay;
-    nix-gaming.url = github:fufexan/nix-gaming;
-    nbfc.url = github:nbfc-linux/nbfc-linux;
+    nix-gaming = {
+      url = github:fufexan/nix-gaming;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nbfc = {
+      url = github:nbfc-linux/nbfc-linux;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, ... }:
-    inputs.flake-parts.lib.mkFlake { inherit self; } {
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" ];
       imports = [
         ./hosts
@@ -25,7 +37,10 @@
       perSystem = { pkgs, system, ... }: rec {
         legacyPackages = import inputs.nixpkgs {
           inherit system;
-          config.allowUnfree = true;
+          config = {
+            allowUnfree = true;
+            allowBroken = true;
+          };
           overlays = [
             inputs.nvim-overlay.overlay
             inputs.emacs-overlay.overlay
@@ -34,10 +49,6 @@
         };
 
         formatter = pkgs.nixpkgs-fmt;
-
-        devShells.default = pkgs.mkShell {
-          packages = [ formatter ];
-        };
       };
     };
 }
